722dc6791dc7254b344ce109857988385010194f
jason4wang <108124132+jason4wang@users.noreply.github.com>
2023-11-22 22:10:21 +0800

    Update percent.js

--- percent.js
+++ percent.js
@@ -1,24 +1,37 @@
-// 判断是否闰年
+// 判断闰年
 function isLeapYear(year) {
-    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-  }
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+
+// 获取今年总天数
+function getTotalDaysOfYear(year) {
+  return isLeapYear(year) ? 366 : 365; 
+}
+
+// 获取今年已过天数
+function getDaysPassedOfYear() {
+  const now = new Date();
+  const startOfYear = new Date(now.getFullYear(), 0, 1);
+  return Math.floor((now - startOfYear) / 86400000); 
+}
+
+// 计算今年过去的百分比
+function getPercentOfYearPassed() {
   
-  // 获取今年天数
-  function getDaysOfThisYear() {
-    const now = new Date();
-    const thisYear = new Date(now.getFullYear(), 0, 1);
-    const diff = now - thisYear;
-    return Math.floor(diff);
-  }
+  const totalDays = getTotalDaysOfYear(new Date().getFullYear());  
+  const daysPassed = getDaysPassedOfYear();
   
-  // 计算百分比
-  function getPercentOfYearPassed() {
-    const days = getDaysOfThisYear();
-    const totalDays = isLeapYear(new Date().getFullYear()) ? 366 : 365;
-    const percent = days / totalDays * 100;
-    return percent.toFixed(8); 
-  }
+  const decimalDigits = 6;
+  const percent = (daysPassed / totalDays * 100).toFixed(decimalDigits);
+
+  return percent;
+}
+
+// 每秒调用一次更新
+setInterval(() => {
   
-  // 调用函数并输出
   const percentPassed = getPercentOfYearPassed();
-  console.log(percentPassed + '%');
+
+  document.getElementById('percent').innerText = percentPassed;
+
+}, 1000);
